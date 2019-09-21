7d2c2cf32e34077ab5e92a18f50c4cbdb06811b7
John Christy <jchristy40@gmail.com>
2019-09-21 12:03:17 -0500

    changes

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import requests
 from flask import Flask
 app = Flask(__name__)
 
+
+
+response = requests.get("https://data.cityofchicago.org/resource/4ijn-s7e5.json")
+#
+
 @app.route('/')
 def hello():
-	return "Hello World"
+	return "response"
 
 @app.route('/<name>')
 def hello_name(name):
